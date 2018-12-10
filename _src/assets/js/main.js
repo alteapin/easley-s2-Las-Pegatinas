5117830c8ee01e90da30b414051c5f13a5e8615d
emma-martin <emmamartinalvarez@gmail.com>
2018-12-10 11:54:44 +0100

    Set local Storage function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -120,7 +120,7 @@ const montseFont = document.getElementById('font-montse');
 const fontCard = document.querySelector('.card-header');
 const fontList = document.querySelector('.fonts-list');
 
-function handleFonttheme(){
+function handleFontTheme(){
     fontCard.classList.remove('font-ubuntu','font-comic','font-montse');
     const checkedAttribute = fontList.querySelector('input[checked]');
 
@@ -143,9 +143,9 @@ function handleFonttheme(){
     }
 }
 
-ubuntuFont.addEventListener('click', handleFonttheme);
-comicFont.addEventListener('click', handleFonttheme);
-montseFont.addEventListener('click', handleFonttheme);
+ubuntuFont.addEventListener('click', handleFontTheme);
+comicFont.addEventListener('click', handleFontTheme);
+montseFont.addEventListener('click', handleFontTheme);
 
 //form
 
@@ -186,8 +186,8 @@ function handlerLinkedin() {
 }
 
 
-//add Image Feature 
-//button div contacto, con div add_image. 
+//add Image Feature
+//button div contacto, con div add_image.
 const uploadBtn = document.querySelector('.button_ad_image');
 const inputImage = document.getElementById('img-selector');
 const boxUserImage = document.querySelector('.card-img');
@@ -218,4 +218,116 @@ linked.addEventListener('keyup', handlerLinkedin);
 
 //API SERVICE
 
+// Localstorage setup
 
+// const localStore = JSON.parse(localStorage.getItem('cardValues')) || 
+const localStore ={
+    'pallete': '',
+    'typography': '',
+    'name': '',
+    'job': '',
+    'photo': '',
+    'email': '',
+    'phone': '',
+    'linkedin': '',
+    'github': '',
+    'skills': [''] //https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/skills.json
+};
+
+//TODO: Usar botones de reset y create sometime.
+const resetBtn = document.querySelector('.reset-btn');
+const createBtn = document.querySelector('.btn-create');
+const formData = document.querySelector('.fill'); // Form se repite y usamos el class .fill porque es unico
+const container = document.querySelector('.selectors-container');
+
+function writeCardToLocalStorage() {
+    
+    function writeData (){
+
+    
+    //probando probando...
+    // event.preventDefault();
+    // Primero buscamos el color de paleta seleccionado
+    const color = colorList.querySelector('input[checked]');
+    localStore.pallete  = color.getAttribute('value');
+
+    // Ahora buscamos la fuente seleccionada
+    const font = fontList.querySelector('input[checked]');
+    localStore.typography = font.getAttribute('value');
+
+    // Ahora vamos a recoger el form de rellena
+    const formName = formData.querySelector('#firstName');
+    localStore.name = formName.value;
+
+    const formJob = formData.querySelector('#job');
+    localStore.job = formJob.value;
+
+    const formPhoto = formData.querySelector('.preview-img');
+    localStore.photo = formPhoto.getAttribute('src');
+
+    const formEmail = formData.querySelector('#email');
+    localStore.email = formEmail.value;
+
+    const formPhone = formData.querySelector('#phone');
+    localStore.phone = formPhone.value;
+
+    const formLinkedin = formData.querySelector('#linkedin');
+    localStore.linkedin = formLinkedin.value;
+
+    const formGithub = formData.querySelector('#github');
+    localStore.github = formGithub.value;
+
+    //GUARDO LOS VALORES EN STRING
+    localStorage.setItem('cardValues', JSON.stringify(localStore));
+    }
+    writeData();
+    //LOS PARSEO, TENGO QUE INYECTARLOS OTRA VEZ EN LA PAGINA PERO NO SE COMO
+    const dataSaved = JSON.parse(localStorage.getItem('cardValues'));
+
+    //
+    if (!!dataSaved === true) {
+        console.log(dataSaved);
+        const palleteChosen = localStore.pallete;
+        const colorSel = palleteChosen.value;
+
+    }
+}
+//HELP https://codepen.io/adalab/pen/WKeOQo?editors=1011
+container.addEventListener('change', writeCardToLocalStorage);
+
+//☝️☝️☝️ con todo este churro consigo que se salven los datos en local pero NO CONSIGO QUE SE PINTEN TRAS F5.
+
+// function handleLocalStorage(){
+    
+//     if (!!dataSaved === true) {
+//         console.log(dataSaved);
+//     }
+//     else {
+//         writeCardToLocalStorage(dataSaved);
+//     }
+// }
+
+// function retrieveDatafromLocalStore () {
+//     const dataSaved = JSON.parse(localStorage.getItem('cardValues'));
+//         dataSaved
+// }
+
+///fuck no consigo que se quede al refrescar la pagina 
+//const savedData = JSON.parse(localStorage.getItem('cardValues'));
+//esto lo he metido como or en el localStore
+
+
+// function retrieveDatafromLocalStore () {
+//     let colorLocal = savedData.pallete;
+
+// }
+
+
+
+
+// let git = document.querySelector('#github');
+// undefined
+// git.value;
+// "kjhkjh"
+// git.value = 'rrrrr';
+// "rrrrr"
